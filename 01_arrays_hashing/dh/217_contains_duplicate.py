132937652a72cafadb85caa4abc73d20b48337da
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-07-23 22:34:33 +0900

    217. contains duplicate

--- 01_arrays_hashing/dh/217_contains_duplicate.py
+++ 01_arrays_hashing/dh/217_contains_duplicate.py
@@ -1,1 +1,14 @@
+from typing import List
+
 # https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        used_nums = {}
+        for i in nums:
+            if i in used_nums:
+                return True
+            used_nums[i] = 1
+        return False
+
+print(Solution().containsDuplicate([1,1,1,3,3,4,3,2,4,2])) # Output: True
