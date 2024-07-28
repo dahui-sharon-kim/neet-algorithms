56a943e77c456970cdead916bbd379206213a993
Dahui Sharon Kim <102566849+dahui-sharon-kim@users.noreply.github.com>
2024-07-29 08:58:20 +0900

    feat: reverse linked list

--- 06_linked_list/dh/206_reverse_linked_list.py
+++ 06_linked_list/dh/206_reverse_linked_list.py
@@ -1,1 +1,30 @@
 # https://leetcode.com/problems/reverse-linked-list/description/
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return to_linked_list(to_list(head)[::-1])
+
+def to_linked_list(lst):
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+
+    for value in lst[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+def to_list(node):
+    lst = []
+    while node:
+        lst.append(node.val)
+        node = node.next
+    return lst
