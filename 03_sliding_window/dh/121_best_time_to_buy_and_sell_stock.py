53fda59c6c66fce68fa0e129826860184379e6bc
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-07-29 00:10:44 +0900

    feat: best time to buy and sell stock

--- 03_sliding_window/dh/121_best_time_to_buy_and_sell_stock.py
+++ 03_sliding_window/dh/121_best_time_to_buy_and_sell_stock.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        min_price = float("inf")
+        max_profit = 0
+        for day_index, price in enumerate(prices):
+            if price < min_price:
+                min_price = price
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
