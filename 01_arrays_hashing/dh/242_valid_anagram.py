87a24f75162e67a9409951f00a37de31c47056b0
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-07-29 00:10:03 +0900

    feat: valid anagram

--- 01_arrays_hashing/dh/242_valid_anagram.py
+++ 01_arrays_hashing/dh/242_valid_anagram.py
@@ -1,1 +1,30 @@
 # https://leetcode.com/problems/valid-anagram/description/
+from collections import deque
+
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    s_dict = {}
+    t_dict = {}
+    for i in s:
+      if i in s_dict:
+        s_dict[i] += 1
+      else:
+        s_dict[i] = 1
+        
+    for i in t:
+      if i in t_dict:
+        t_dict[i] += 1
+      else:
+        t_dict[i] = 1
+    
+    for i in t_dict:
+      if not (i in s_dict) or s_dict[i] != t_dict[i]:
+        return False
+    for i in s_dict:
+      if not (i in t_dict) or s_dict[i] != t_dict[i]:
+        return False
+    
+    return True
+
+print(Solution().isAnagram("ab", "a"))
+print(Solution().isAnagram("a", "ab"))
