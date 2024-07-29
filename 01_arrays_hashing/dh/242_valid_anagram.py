5d87710e01c8198ba2fd8040fca9db663d929eb0
Dahui Sharon Kim <102566849+dahui-sharon-kim@users.noreply.github.com>
2024-07-29 23:56:28 +0900

    fix: valid anagram

--- 01_arrays_hashing/dh/242_valid_anagram.py
+++ 01_arrays_hashing/dh/242_valid_anagram.py
@@ -16,15 +16,8 @@ class Solution:
         t_dict[i] += 1
       else:
         t_dict[i] = 1
-    
-    for i in t_dict:
-      if not (i in s_dict) or s_dict[i] != t_dict[i]:
-        return False
-    for i in s_dict:
-      if not (i in t_dict) or s_dict[i] != t_dict[i]:
-        return False
-    
-    return True
+
+    return t_dict == s_dict
 
 print(Solution().isAnagram("ab", "a"))
 print(Solution().isAnagram("a", "ab"))
