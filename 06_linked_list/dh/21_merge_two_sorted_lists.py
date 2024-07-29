c16847eb899344b9346f9197cc2bd4a8402cd535
Dahui Sharon Kim <102566849+dahui-sharon-kim@users.noreply.github.com>
2024-07-29 15:36:15 +0900

    feat: update merge two sorted lists

--- 06_linked_list/dh/21_merge_two_sorted_lists.py
+++ 06_linked_list/dh/21_merge_two_sorted_lists.py
@@ -7,58 +7,28 @@ class ListNode:
         self.next = next
 
 class Solution:
-  def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:  
-        dummy = ListNode()
-        current = dummy
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        plain_list = []
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
+        while list1 or list2:
+            if not list2 or (list1 and list1.val < list2.val):
+                plain_list.append(list1.val)
                 list1 = list1.next
             else:
-                current.next = list2
+                plain_list.append(list2.val)
                 list2 = list2.next
-
-            current = current.next
-
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-
-        return dummy.next
+        
+        return to_linked_list(plain_list)
+            
 
 def to_linked_list(lst):
     if not lst:
         return None
-    head = ListNode(lst[0])
-    current = head
+    head = ListNode(lst[0]) 
+    current = head  
+    
     for value in lst[1:]:
-        current.next = ListNode(value)
+        current.next = ListNode(value) 
         current = current.next
+
     return head
-
-def to_list(node):
-    result = []
-    while node:
-        result.append(node.val)
-        node = node.next
-    return result
-
-
-solution = Solution()
-
-list1 = to_linked_list([1, 2, 4])
-list2 = to_linked_list([1, 3, 4])
-merged_list_head = solution.mergeTwoLists(list1, list2)
-print(to_list(merged_list_head))  # Output: [1, 1, 2, 3, 4, 4]
-
-list1 = to_linked_list([])
-list2 = to_linked_list([])
-merged_list_head = solution.mergeTwoLists(list1, list2)
-print(to_list(merged_list_head))  # Output: []
-
-list1 = to_linked_list([])
-list2 = to_linked_list([0])
-merged_list_head = solution.mergeTwoLists(list1, list2)
-print(to_list(merged_list_head))  # Output: [0]
