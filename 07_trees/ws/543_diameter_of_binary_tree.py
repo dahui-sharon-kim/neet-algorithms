3c2664c0ebeced343a017226869a0831ff901638
WooSeong Chung <jungyh0528@gmail.com>
2024-07-30 22:47:43 +0900

    feat: implement diameter of binary tree

---
+++ 07_trees/ws/543_diameter_of_binary_tree.py
@@ -0,0 +1,71 @@
+from collections import deque
+import sys
+from typing import Optional
+from tree import TreeNode, create_binary_tree, print_tree
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        result = 0
+        if not root:
+            return result
+
+        stack = deque()
+        stack.append((root, 1))
+        while stack:
+            node, depth = stack.pop()
+            result = max(result, depth)
+
+            if node.left:
+                stack.append((node.left, depth + 1))
+            if node.right:
+                stack.append((node.right, depth + 1))
+
+        return result
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        result = 0
+        stack = deque()
+        stack.append(root)
+
+        while stack:
+            node = stack.pop()
+            left, right = 0, 0
+            if node.left:
+                left = self.maxDepth(node.left)
+                stack.append(node.left)
+            if node.right:
+                right = self.maxDepth(node.right)
+                stack.append(node.right)
+
+            result = max(result, left + right)
+
+        return result
+
+    def diameterOfBinaryTree2(self, root: Optional[TreeNode]) -> int:
+        def depth(node: Optional[TreeNode]):
+            if not node:
+                return 0
+            return max(depth(node.left), depth(node.right)) + 1
+
+        result = 0
+        stack = deque()
+        stack.append(root)
+
+        while stack:
+            node = stack.pop()
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+            result = max(result, depth(node.left) + depth(node.right))
+
+        return result
+
+
+if __name__ == "__main__":
+    nodes = list(map(int, sys.argv[1:]))
+    root = create_binary_tree(nodes)
+    print_tree(root)
+    print(Solution().diameterOfBinaryTree(root))
