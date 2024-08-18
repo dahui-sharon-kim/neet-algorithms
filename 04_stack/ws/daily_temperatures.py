5eff72f4044cd0b0e6ff824cdabea2c1f875d315
WooSeong Chung <jungyh0528@gmail.com>
2024-08-18 21:03:51 +0900

    feat: implement daily temperatures

---
+++ 04_stack/ws/daily_temperatures.py
@@ -0,0 +1,22 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        results = [0 for _ in range(len(temperatures))]
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < t:
+                if stack[-1][0] < t:
+                    _, index = stack.pop()
+                    results[index] = i - index
+            stack.append((t, i))
+
+        return results
+
+
+if __name__ == "__main__":
+    temps = list(map(int, sys.argv[1:]))
+    print(Solution().dailyTemperatures(temps))
