35df3e639759a177a9cba9a95771e4174949fa12
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-08-23 23:18:26 +0900

    feat: two some 2 input array is sorted

---
+++ 02_two_pointers/dh/167_two_some_ii_input_array_is_sorted.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        
+        while left < right:
+            current_sum = numbers[left] + numbers[right]
+            
+            if current_sum == target:
+                return [left + 1, right + 1]
+            elif current_sum < target:
+                left += 1 
+            else:
+                right -= 1
+                
+        return []  
