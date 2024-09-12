f8bf5edfee48bdb5535edf7ee231351ae8d5a3a3
Dahui Sharon Kim <102566849+dahui-sharon-kim@users.noreply.github.com>
2024-09-12 14:47:56 +0900

    feat: longest repeating character replacement

---
+++ 03_sliding_window/dh/424_longest_repeating_character_replacement.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/longest-repeating-character-replacement/
+
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        letters = defaultdict(int)
+        length = len(s)
+        maximum = 0
+        left = 0
+        maximum_length = 0
+
+        for right in range(length):
+            letters[s[right]] += 1
+            maximum = max(maximum, letters[s[right]])
+
+            if (right - left + 1) - maximum > k:
+                if letters[s[left]] > 1:
+                    defaultdict[s[left]] -= 1
+                else:
+                    del defaultdict[s[left]]
+                left += 1
+
+            maximum_length = max(maximum_length, right - left + 1) # 어렵다..
+        
+
