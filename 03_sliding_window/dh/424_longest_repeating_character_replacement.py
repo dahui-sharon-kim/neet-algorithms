afbc0a601bd087f3fe81a84a446a89dc062266f4
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-09-19 18:43:10 +0900

    fix: solve again

--- 03_sliding_window/dh/424_longest_repeating_character_replacement.py
+++ 03_sliding_window/dh/424_longest_repeating_character_replacement.py
@@ -3,23 +3,22 @@
 from collections import defaultdict
 
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        letters = defaultdict(int)
-        maximum = 0
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ans = 0
         left = 0
-        maximum_length = 0
+        letters = defaultdict(int)
+
+        if (len(s) == 1):
+            return 1
 
         for right in range(len(s)):
+            print(s[right])
             letters[s[right]] += 1
-            maximum = max(maximum, letters[s[right]])
-
-            if (right - left + 1) - maximum > k:
-                if letters[s[left]] > 1:
-                    defaultdict[s[left]] -= 1
-                else:
-                    del defaultdict[s[left]]
+            while letters[s[right]] > 1:
+                letters[s[left]] -= 1
+                if letters[s[left]] == 0:
+                    del letters[s[left]]
                 left += 1
+            ans = max(ans, right-left+1)
 
-            maximum_length = max(maximum_length, right - left + 1) # 어렵다..
-        
-
+        return ans
