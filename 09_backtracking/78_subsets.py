ce87cffdf3b92c14dabf7e0bf3c32ce4ff50baad
dahui-sharon-kim <dahui.s.kim@gmail.com>
2024-09-25 00:35:21 +0900

    feat: subsets

---
+++ 09_backtracking/78_subsets.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+        dfs(0)
+        return res
+            
+    
+print(Solution().subsets([1,2,3]))
