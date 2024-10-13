b67e606a017e82149bb619a340ce11f421909eea
WooSeong Chung <jungyh0528@gmail.com>
2024-10-13 15:49:15 +0900

    feat: insert interval

---
+++ 16_intervals/ws/insert_interval.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                res.extend(intervals[i:])
+                return res
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
