18db7b5c86aca5e189eeb72f5c47d4da27c68260
WooSeong Chung <jungyh0528@gmail.com>
2024-10-21 23:31:48 +0900

    feat: maximum subarray

---
+++ 15_greedy/ws/maximum_subarray.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        total = 0
+
+        for n in nums:
+            total += n
+            res = max(res, total)
+            if total < 0:
+                total = 0
+        return res
